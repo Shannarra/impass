2a458d50d20a9668c01adf7db015f8ed386da1f4
shannarra <petarangelov15@gmail.com>
2024-10-03 11:31:56 +0300

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-mod util;
 mod config;
+mod util;
 
 // fn write_image(content: &mut Vec<u8>, output: &String) {
 // 		if let Some(idx) = util::index_vec(content, &util::constants::EOF_SIGNATURE) {
@@ -9,29 +9,29 @@ mod config;
 // 		} else {
 // 				error!("Could not find PNG EOF signature!");
 // 		}
-		
+
 // 		let output: &std::path::Path = std::path::Path::new(output);
-		
+
 // 		let _ = std::fs::write(output, &content);
-		
+
 // 		println!("{content:?}");
 
-// 		println!("Done!");		
+// 		println!("Done!");
 // }
 
 mod reading;
 
 fn main() {
-		let argv: Vec<String> = std::env::args().collect();
-		let config = config::Config::from_args(&argv);
+    let argv: Vec<String> = std::env::args().collect();
+    let config = config::Config::from_args(&argv);
 
-		let mut content = Vec::new(); 
-		let index: usize = reading::gimme_bytecode(&config, &mut content);
+    let mut content = Vec::new();
+    let index: usize = reading::gimme_bytecode(&config, &mut content);
 
-		if config.in_file.is_empty() {
-				let input = util::prompt("Enter your secret");
-				println!("Input: {input}");
-		} else {
-				println!("Decoding... {}", config.in_file);
-		}
+    if config.in_file.is_empty() {
+        let input = util::prompt("Enter your secret");
+        println!("Input: {input}");
+    } else {
+        println!("Decoding... {}", config.in_file);
+    }
 }
